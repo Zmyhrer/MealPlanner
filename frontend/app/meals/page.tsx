a1424e311e2bc802c353de33ebafee40ca11cf35
Zmyhrer <zachs.myhrer@gmail.com>
2025-12-17 00:43:21 -0600

    feat: add icon and search input in searchBar.tsx

--- frontend/app/meals/page.tsx
+++ frontend/app/meals/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "../../styles/meals.module.css";
+import SearchBar from "@/components/searchBar";
 
 const page = () => {
   return (
     <div>
       <div className={styles["grid-container"]}>
         <div className={styles["grid-item-1"]}>Meals</div>
-        <div className={styles["grid-item-2"]}>Search Bar</div>
+        <div className={styles["grid-item-2"]}>
+          <SearchBar />
+        </div>
         <div className={styles["grid-item-3"]}></div>
       </div>
     </div>
--- frontend/components/searchBar.tsx
+++ frontend/components/searchBar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../styles/searchBar.module.css";
 
 const searchBar = () => {
-  return <div></div>;
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["icon"]}>
+        <Image src="/search.png" alt="search icon" width={20} height={20} />
+      </div>
+      <input type="search" className={styles["searchInput"]} />
+    </div>
+  );
 };
 
 export default searchBar;
