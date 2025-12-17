f133bfecb7e309c76689f4695c4813a44e428076
Zmyhrer <zachs.myhrer@gmail.com>
2025-12-17 00:18:08 -0600

    feat: add css grid to meals page

--- frontend/app/meals/page.tsx
+++ frontend/app/meals/page.tsx
@@ -5,9 +5,9 @@ const page = () => {
   return (
     <div>
       <div className={styles["grid-container"]}>
-        <div className={styles["grid-item grid-item-1"]}>Meals</div>
-        <div className={styles["grid-item grid-item-2"]}></div>
-        <div className={styles["grid-item grid-item-3"]}></div>
+        <div className={styles["grid-item-1"]}>Meals</div>
+        <div className={styles["grid-item-2"]}>Search Bar</div>
+        <div className={styles["grid-item-3"]}></div>
       </div>
     </div>
   );
