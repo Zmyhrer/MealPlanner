afbf3009752a8e19a0359759edbf7e87ba2f6cf2
Zmyhrer <zachs.myhrer@gmail.com>
2025-12-29 11:09:08 -0600

    feat: add styling prop to (containerLabel)

--- frontend/components/containerLabel.tsx
+++ frontend/components/containerLabel.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "@/styles/containerLabel.module.css";
+import { Style } from "util";
 
 interface ContainerLabelProps {
   label: string;
+  style?: React.CSSProperties;
   children: React.ReactNode;
 }
 
-const ContainerLabel: React.FC<ContainerLabelProps> = ({ label, children }) => {
+const ContainerLabel: React.FC<ContainerLabelProps> = ({
+  label,
+  style,
+  children,
+}) => {
   return (
     <div className={styles.container}>
-      <div className={styles.label}>{label}</div>
+      <div className={styles.label} style={style}>
+        {label}
+      </div>
       {children}
     </div>
   );
